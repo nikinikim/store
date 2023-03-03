3616c739cbd41ab3c510aeb7d55689bf731a488b
Николай <colt92@inbox.ru>
2023-03-03 19:59:53 +0500

    Initial commit

---
+++ src/test/java/me/project/socksstoreapp/SocksStoreAppApplicationTests.java
@@ -0,0 +1,13 @@
+package me.project.socksstoreapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocksStoreAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
